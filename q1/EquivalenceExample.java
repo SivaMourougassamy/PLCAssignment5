c7a808d4c30c3a053cca7fc544e37770a9fa6ef8
SivaMourougassamy <56765191+SivaMourougassamy@users.noreply.github.com>
2020-04-17 23:04:28 -0400

    Add files via upload

---
+++ q1/EquivalenceExample.java
@@ -0,0 +1,9 @@
+public class EquivalenceExample{
+    public static void main(String []args){
+        int integerNum = 2;
+        double doubleNum = 0.0;
+        
+        doubleNum = integerNum;
+        
+        System.out.println(doubleNum);
+    }
